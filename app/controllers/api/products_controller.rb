69b4015a8b0894f6752c29ae6e0598e28a8f674b
GinaMaddox <gina.maddox@gmail.com>
2018-07-04 13:18:00 -0400

    syntax changes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,18 +1,18 @@
 class Api::ProductsController < ApplicationController
-  def all_products_action
-    @products = Product.all
-    render "all_products_view.json.jbuilder"
-  end
+  # def all_products_action
+  #   @products = Product.all
+  #   render "all_products_view.json.jbuilder"
+  # end
 
-  def first_product_action
-    @product = Product.first
-    render "first_product_view.json.jbuilder"
-  end
+  # def first_product_action
+  #   @product = Product.first
+  #   render "first_product_view.json.jbuilder"
+  # end
 
-  def second_product_action
-    @product = Product.second
-    render "second_product_view.json.jbuilder"
-  end
+  # def second_product_action
+  #   @product = Product.second
+  #   render "second_product_view.json.jbuilder"
+  # end
 
   def index
     @products = Product.all
